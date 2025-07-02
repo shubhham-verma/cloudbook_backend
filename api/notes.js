7b3deb760a95e8eaa6ea25e613c685572471e2a8
shubhham-verma <108178291+shubhham-verma@users.noreply.github.com>
2025-07-03 00:07:29 +0530

    Changed to deploy to vercel 8

--- api/notes.js
+++ api/notes.js
@@ -2,6 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Or your Netlify URL
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+    next();
+});
+
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
